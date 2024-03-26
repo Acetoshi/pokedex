9f24cb46081ae64368b1a32e9e761ac35dcb4fb0
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-03-26 16:03:02 +0100

    added some dynamic capability to component

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,23 @@
+const pokemonList = [
+
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+
+];
+
 export default function PokemonCard() {
+     const pokemon = pokemonList[1];
 
     return <figure> 
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbizarre"/>
-        <figcaption>Bulbizarre</figcaption>
-        </figure>;
-
+        {(pokemon.imgSrc)?<img src={pokemon.imgSrc} alt="bulbizarre"/>:<p>MissingNo</p>}
+        <figcaption>{pokemon.name}</figcaption>
+        </figure >;
 }
 
 
